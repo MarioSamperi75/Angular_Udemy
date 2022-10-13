696b195a90989218de38c876dafda9cb8c4c8efe
Mario Samperi <mariosamperi@Marios-MacBook-Pro.local>
2022-10-13 14:57:00 +0200

    Binding to Directive Properties

--- ng05_directives-start/src/app/HighlightElement2/highlight-element2.directive.ts
+++ ng05_directives-start/src/app/HighlightElement2/highlight-element2.directive.ts
@@ -1,28 +1,31 @@
-import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, Input, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightElement2]'
 })
 export class HighlightElement2Directive implements OnInit{
 
-  @HostBinding('style.backgroundColor') backgroundColor: string = "transparent";
+  @HostBinding('style.backgroundColor') backgroundColor: string;
+  @Input() defaultColor: string = 'transparent';
+  @Input() highlightedColor: string = 'blue   ';
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2 ) {
    }
 
 
   ngOnInit() {
+    this.backgroundColor = this.defaultColor;
   }
 
  // you can use all dom events
  // for a list visit:  https://www.w3schools.com/jsref/dom_obj_event.asp
   @HostListener('mouseenter') addStyle(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange')
-    this.backgroundColor = "LightGreen";
+    this.backgroundColor = this.highlightedColor;
   }
 
   @HostListener('mouseleave') removeStyle(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent')
-    this.backgroundColor = "transparent";
+    this.backgroundColor = this.defaultColor;
   }
 }
