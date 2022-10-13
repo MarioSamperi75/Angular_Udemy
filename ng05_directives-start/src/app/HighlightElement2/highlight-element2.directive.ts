093a0b763e903de776c99077e8bf78db16539afa
Mario Samperi <mariosamperi@Marios-MacBook-Pro.local>
2022-10-13 13:53:15 +0200

    Using HostBinding to Bind instance variables to Host Properties

--- ng05_directives-start/src/app/HighlightElement2/highlight-element2.directive.ts
+++ ng05_directives-start/src/app/HighlightElement2/highlight-element2.directive.ts
@@ -1,24 +1,28 @@
-import { Directive, ElementRef, HostListener, OnInit, Renderer2 } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener, OnInit, Renderer2 } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightElement2]'
 })
 export class HighlightElement2Directive implements OnInit{
 
-  constructor(private elementRef: ElementRef, private renderer: Renderer2 ) {
+  @HostBinding('style.backgroundColor') backgroundColor: string = "transparent";
 
+  constructor(private elementRef: ElementRef, private renderer: Renderer2 ) {
    }
+
+
   ngOnInit() {
-   
   }
 
  // you can use all dom events
  // for a list visit:  https://www.w3schools.com/jsref/dom_obj_event.asp
   @HostListener('mouseenter') addStyle(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange')
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'orange')
+    this.backgroundColor = "LightGreen";
   }
 
   @HostListener('mouseleave') removeStyle(eventData: Event) {
-    this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent')
+    // this.renderer.setStyle(this.elementRef.nativeElement, 'background-color', 'transparent')
+    this.backgroundColor = "transparent";
   }
 }
