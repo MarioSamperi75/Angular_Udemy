3ce3526d01693167b8698f130299fa57b5d16f6f
Mario Samperi <mariosamperi@Marios-MacBook-Pro.local>
2022-11-17 15:19:46 +0100

    shoppingListService

--- recpies-app/src/app/shopping-list/shopping-list.service.ts
+++ recpies-app/src/app/shopping-list/shopping-list.service.ts
@@ -1,6 +1,20 @@
 import { Injectable } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 @Injectable({providedIn:'root'})
 export class ShoppingListService {
+
+    private ingredients: Ingredient[] = [
+        new Ingredient ("Pomodori", 10),
+        new Ingredient ("Carne Macinata", 20),
+      ];
+
+      getIngredients(){
+        return this.ingredients;
+      }
+
+      addIngredient (ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+      }
 
 }
