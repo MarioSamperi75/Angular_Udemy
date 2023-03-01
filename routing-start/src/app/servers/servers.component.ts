d12fd0226ba95d2b0d7d41600cd792e521dfced4
Mario Samperi <mariosamperi@Marios-MacBook-Pro.local>
2023-03-01 15:14:05 +0100

    Using relative paths in programmating navigation

--- routing-start/src/app/servers/servers.component.ts
+++ routing-start/src/app/servers/servers.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Route, Router } from '@angular/router';
 import { ServersService } from './servers.service';
 
 @Component({
@@ -7,12 +8,24 @@ import { ServersService } from './servers.service';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
+
   public servers: {id: number, name: string, status: string}[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(private serversService: ServersService, 
+              private router:Router,
+              private route: ActivatedRoute) { }
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
+
+  onReload() {
+    console.log("ONRELOADFUNKAR!!!")
+    // navigate using relative path :the default is the root
+    // != routingLink. routingLink as the actual path as default
+    // to set the actual path please inject ActivatedRoute and use relativeTo
+    // this route is not working anymore, you get sever/server that do not exist
+    this.router.navigate(["servers"], {relativeTo: this.route});
+    }
 
 }
