2c0c0c1364d28ef92891bc14320a1f0a70ad30b2
Mario Samperi <mariosamperi@marios-mbp.home>
2023-03-03 10:25:33 +0100

    creating and connecting  not-found page

--- routing-start/src/app/app.module.ts
+++ routing-start/src/app/app.module.ts
@@ -11,13 +11,13 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'users', component: UsersComponent, children : [
     { path: ':id/:name', component: UserComponent },
   ] },
- 
   {
     path: 'servers',
     component: ServersComponent,
@@ -26,7 +26,12 @@ const appRoutes: Routes = [
       { path: ':id/edit', component: EditServerComponent },
     ],
   },
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: "/not-found" }
 ];
+
+// remember to have ** at the end.
+// ** means "the other paths not yet specified"
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,6 +41,7 @@ const appRoutes: Routes = [
     UserComponent,
     EditServerComponent,
     ServerComponent,
+    PageNotFoundComponent,
   ],
   imports: [BrowserModule, FormsModule, RouterModule.forRoot(appRoutes)],
   providers: [ServersService],
